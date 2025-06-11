df599787772b7f9bb1247611f6fbba0b00d612f8
左维杰 <zuoweijie2017@gmail.com>
2025-06-11 10:36:49 +0800

    解决启动就报错的问题

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3731000948"
+    serviceWorkerVersion: "2871653430"
   }
 });
